b1a0ba19bc492c26a8d49a566a9941898004b842
Patrick Sanan <patrick.sanan@gmail.com>
2023-10-22 13:44:31 +0200

    .

---
+++ website_fun/scripts/html_update.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+"""Utilities to automatically update HTML source"""
+
+import os
+import argparse
+
+HEADER_TAG_PREFIX = "<!--END HEADER"
+FOOTER_TAG_PREFIX = "<!--START FOOTER"
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+HEADER_TEMPLATE_PATH = os.path.join(THIS_DIR, "..", "templates", "header.html")
+FOOTER_TEMPLATE_PATH = os.path.join(THIS_DIR, "..", "templates", "footer.html")
+
+
+def update_header_and_footer(path, header_lines, footer_lines):
+    """Overwrite header and footer, if custom comments are found."""
+    lines_out = []
+    if not path.endswith(".html"):
+        raise Exception(f"{path} isn't an HTML file")
+    with open(path, "r") as f:
+        header_comment_found = False
+        footer_comment_found = False
+        for line in f:
+            if not header_comment_found:
+                header_comment_found = line.lstrip().startswith(
+                    "<!--END HEADER")
+                if header_comment_found:
+                    lines_out.extend(header_lines)
+                    continue
+            if header_comment_found and not footer_comment_found:
+                footer_comment_found = line.lstrip().startswith(
+                    "<!--START FOOTER")
+            if footer_comment_found:
+                lines_out.extend(footer_lines)
+                break
+            if header_comment_found:
+                lines_out.append(line)
+        if not header_comment_found:
+            print(
+                f"WARNING: no header comment ({HEADER_TAG_PREFIX}) found. {path} will not be updated"
+            )
+        elif not footer_comment_found:
+            print(
+                f"WARNING: no footer comment ({FOOTER_TAG_PREFIX}) found. footer will not be updated for {path}"
+            )
+    with open(path, "w") as f:
+        f.writelines(lines_out)
+
+
+def _update_directory(directory):
+    """Update all HTML files in a directory"""
+    if not os.path.isdir(directory):
+        raise Exception(f"{directory} is not a directory")
+    with open(HEADER_TEMPLATE_PATH, "r") as f:
+        header_lines = f.readlines()
+    with open(FOOTER_TEMPLATE_PATH, "r") as f:
+        footer_lines = f.readlines()
+    for filename in os.listdir(directory):
+        if not filename.endswith(".html"):
+            continue
+        print(f"Attempting to update {filename}")
+        update_header_and_footer(os.path.join(directory, filename), header_lines, footer_lines)
+
+
+def update_main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', '-d', required=True)
+    args = parser.parse_args()
+
+    _update_directory(args.directory)
+
+
+if __name__ == "__main__":
+    update_main()
